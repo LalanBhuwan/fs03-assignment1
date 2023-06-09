6805fc142034024c65d8ba6159b305d2b1924538
LalanBhuwan <bhuwanlalan475@gmail.com>
2023-06-09 22:04:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <h1>Learn web development</h1>
+     <p>Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.
+      
+     </p>
+     <br/>
+     <p>The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+     <br/>
+     <p>If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web develpment (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how wib technologist work.</p>
     </div>
   );
 }
